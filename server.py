26d8b940c61f8509f43b7d715c5d684a9445c2c2
xaviSalazar <xavicoel@gmail.com>
2022-02-22 10:09:14 +0100

    fix: nltk txt file

--- server.py
+++ server.py
@@ -25,8 +25,8 @@ import signal
 # Response message server
 async def server(websocket):
     async for message in websocket:
-        # answer = get_response(message)
-        answer = 'prueba envio'
+        answer = get_response(message)
+        # answer = 'prueba envio'
         await websocket.send(answer)
 
 async def main():
